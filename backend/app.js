db3e1533929730aba555b2e4e2e72c10a0e48641
rehanulhb <rehanulh@gmail.com>
2025-12-24 00:24:04 +0100

    Backend Added, with Mongodb Model

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Server Running");
+});
+
+async function connectDb() {
+  mongoose.connect("mongodb://cluster0.lli8l52.mongodb.net", {
+    dbName: "mean_ecommerce",
+  });
+  console.log("MongoDb Connected");
+}
+
+connectDb().catch((err) => {
+  console.log(err);
 });
 
 app.listen(port, () => {
