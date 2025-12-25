91f3b5a928ea2b618321d4d43e85b4e38ceeda2e
rehanulhb <rehanulh@gmail.com>
2025-12-25 02:40:10 +0100

    Home and Category Component Created in Frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Categories } from './components/manage/categories/categories';
+import { CategoryForm } from './components/manage/category-form/category-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'admin/categories',
+    component: Categories,
+  },
+  {
+    path: 'admin/categories/add',
+    component: CategoryForm,
+  },
+  {
+    path: 'admin/categories/:id',
+    component: CategoryForm,
+  },
+];
---
+++ frontend/src/app/components/manage/category-form/category-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-form',
+  imports: [],
+  templateUrl: './category-form.html',
+  styleUrl: './category-form.scss',
+})
+export class CategoryForm {
+
+}
