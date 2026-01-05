5e071b5313029b86d07e00f843913c252d2f8744
rehanulhb <rehanulh@gmail.com>
2026-01-05 23:49:37 +0100

    Product Services Created

--- frontend/src/app/services/product.ts
+++ frontend/src/app/services/product.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environmentProd } from '../../environments/environment.development';
+import { TProduct } from '../types/product';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,19 @@ export class Product {
   http = inject(HttpClient);
 
   getAllProducts() {
-    return this.http.get(environmentProd.apiUrl + '/product');
+    return this.http.get<TProduct[]>(environmentProd.apiUrl + '/product');
+  }
+  getProductById(id: string) {
+    return this.http.get<TProduct>(environmentProd.apiUrl + '/product' + id);
+  }
+
+  addProduct(model: TProduct) {
+    return this.http.post(environmentProd.apiUrl + '/product', model);
+  }
+  updateProduct(id: string, model: TProduct) {
+    return this.http.put(environmentProd.apiUrl + '/product/' + id, model);
+  }
+  deleteProduct(id: string) {
+    return this.http.delete(environmentProd.apiUrl + '/product/' + id);
   }
 }
--- frontend/src/app/types/product.ts
+++ frontend/src/app/types/product.ts
@@ -1,9 +1,10 @@
-export interface Product {
-  name: 'Iphone 17 Pro';
-  shortDescription: 'This is Iphone 17 Pro';
-  description: 'This is Long Description fkjsdkjsdfksdfjk';
-  Price: 1200;
-  discount: 20;
-  images: ['fdjslkfjdksfkls'];
-  categoryId: '694c8d5e1e4a649b3e50ab8d';
+export interface TProduct {
+  id?: string;
+  name: string;
+  shortDescription: string;
+  description: string;
+  Price: number;
+  discount: number;
+  images: string[];
+  categoryId: string;
 }
