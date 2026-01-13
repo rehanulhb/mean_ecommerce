9ffc48bf017950d0be3bc0d3350f6f39154f1db4
rehanulhb <rehanulh@gmail.com>
2026-01-13 23:47:52 +0100

    Working on Category and Brand Dropdown field shwoing on add to Product page

--- frontend/src/app/components/manage/category-form/category-form.ts
+++ frontend/src/app/components/manage/category-form/category-form.ts
@@ -4,10 +4,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { Category } from '../../../services/category';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-category-form',
-  imports: [FormsModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, MatInputModule, MatButtonModule, MatSelectModule],
   templateUrl: './category-form.html',
   styleUrl: './category-form.scss',
 })
--- frontend/src/app/components/manage/product-form/product-form.ts
+++ frontend/src/app/components/manage/product-form/product-form.ts
@@ -2,10 +2,15 @@ import { Component, inject } from '@angular/core';
 import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { TBrand } from '../../../types/brand';
+import { TCategory } from '../../../types/category';
+import { Category } from '../../../services/category';
+import { Brand } from '../../../services/brand';
 
 @Component({
   selector: 'app-product-form',
-  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, MatSelectModule],
   templateUrl: './product-form.html',
   styleUrl: './product-form.scss',
 })
@@ -19,9 +24,34 @@ export class ProductForm {
     discount: [],
     images: this.formbuilder.array([]),
     categoryId: [null, [Validators.required]],
+    brandId: [null, [Validators.required]],
   });
 
-  addProduct() {}
+  categoryService = inject(Category);
+  brandService = inject(Brand);
+
+  brands: TBrand[] = [];
+  categories: TCategory[] = [];
+
+  ngOnInit() {
+    this.addImage();
+    this.categoryService.getCategories().subscribe((result) => {
+      this.categories = result;
+    });
+    this.brandService.getBrands().subscribe((result) => {
+      this.brands = result;
+    });
+  }
+  addProduct() {
+    let value = this.productForm.value;
+    console.log(value);
+  }
+  addImage() {
+    this.images.push(this.formbuilder.control(null));
+  }
+  removeImage() {
+    this.images.removeAt(this.images.controls.length - 1);
+  }
   get images() {
     return this.productForm.get('images') as FormArray;
   }
